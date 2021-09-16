5d96e6ae29d1554c3be6191c537c3e96b6bd381f
zhouboming <zhou.boming@gmail.com>
2021-09-16 16:16:07 -0400

    javascript additions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to My Site!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
